814e689230b9b5d9748c54460724647e20212e26
Daniel Jordan <daniel.jordan@fellow-consulting.de>
2022-04-26 15:47:18 +0200

    Change Title

--- packages/editor-ui/src/views/TemplatesWorkflowView.vue
+++ packages/editor-ui/src/views/TemplatesWorkflowView.vue
@@ -115,10 +115,10 @@ export default mixins(workflowHelpers).extend({
 	watch: {
 		template(template: ITemplatesWorkflowFull) {
 			if (template) {
-				setPageTitle(`n8n - Template template: ${template.name}`);
+				setPageTitle(`Workflow² - Template template: ${template.name}`);
 			}
 			else {
-				setPageTitle(`n8n - Templates`);
+				setPageTitle(`Workflow² - Templates`);
 			}
 		},
 	},
