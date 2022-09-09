f9a6fdf7005784954cc03ba5ae3faa0f9e0ad6ec
Benjamin Roder <Benjamin.Roder@fellow-consulting.de>
2022-09-09 14:26:51 +0200

    changed extractionnode

--- packages/nodes-base/nodes/Doc2App/Extract/Extract.node.ts
+++ packages/nodes-base/nodes/Doc2App/Extract/Extract.node.ts
@@ -1,5 +1,7 @@
 import {
-    IExecuteFunctions,
+	IHookFunctions,
+	IWebhookFunctions,
+	IExecuteFunctions
 } from 'n8n-core';
 
 import {
@@ -9,6 +11,10 @@ import {
     INodeType,
     INodeTypeDescription,
     IDataObject,
+		ILoadOptionsFunctions,
+		INodePropertyOptions,
+    IHttpRequestOptions,
+    IWebhookResponseData,
 } from 'n8n-workflow';
 
 import {
@@ -44,100 +50,118 @@ export class Extract implements INodeType {
             },
         ],
         properties: [
-            {
-				displayName: 'Binary Property',
+					{
+						displayName: 'Label',
+						name: 'label',
+						type: 'string',
+						default: 'Test_Label',
+						description: 'Lable of the Document',
+						required: false,
+					},
+					{
+						displayName: 'Binary Property',
 
-				name: 'binaryPropertyName',
-				type: 'string',
-				default: 'data',
-				description: 'Object property name which holds binary data.',
-				required: true,
+						name: 'binaryPropertyName',
+						type: 'string',
+						default: 'data',
+						description: 'Object property name which holds binary data',
+						required: true,
+					},
+					{
+						displayName: 'Documenttype',
+						name: 'doc_type',
+						type: 'options',
+						typeOptions: {
+							loadOptionsMethod: 'getDocumentTypes',
+						},
+						description: 'Use documenttype from the list',
+						default: '',
+						required: false,
+					},
+					{
+						displayName: 'Documentsubtype',
+						name: 'doc_sub_type',
+						type: 'options',
+						displayOptions: {
+							show: {
+								doc_type: [
+									'INVOICE'
+								]
+							},
+						},
+						options: [
+							{
+								name: 'All',
+								value: ''
+							},
+							{
+								name: 'Warenrechnung',
+								value: 'GOODS_INVOICE'
+							},
+							{
+								name: 'Kostenrechnung',
+								value: 'COST_INVOICE'
+							},
+							{
+								name: 'Frachtrechnung',
+								value: 'FREIGHT_INVOICE'
+							},
+						],
+						default: '',
+						description: 'Set document subtype',
+						required: true,
+					},
+      	],
+    	};
+		methods = {
+			loadOptions: {
+					async getDocumentTypes(this: IHookFunctions | ILoadOptionsFunctions): Promise<INodePropertyOptions[]>{
+							const returnData = [
+									{
+											name: 'All',
+											value: '',
+									}
+							];
+							const credentials = await this.getCredentials('Doc2AppApi') as IDataObject;
+							const api_key = credentials.apiKey;
+							let url = api.get_document_types;
+							const options: IHttpRequestOptions = {
+									url,
+									headers: {
+											'Content-Type': 'application/json',
+											'X-API-KEY': api_key,
+									},
+									method: 'GET',
+									body:  {},
+									json: true,
+							};
+
+							try {
+									if (this.helpers === undefined) {
+											return returnData;
+									}
+									const response = await this.helpers.httpRequest(options);
+									for (const document_type of response.data) {
+											const docTypeName = document_type.title;
+											const docTypeId = document_type.document_key;
+
+											if (document_type.is_active !== true) {
+													continue;
+											}
+											returnData.push({
+													name: docTypeName,
+													value: docTypeId,
+											});
+									}
+							} catch(e: any) {
+									console.error(e);
+									// throw new Error('Some internal error occur. Please try again later');
+							}
+
+							return returnData;
+					},
 			},
-			{
-				displayName: 'Documenttype',
-				name: 'doc_type',
-				type: 'options',
-				options: [
-					{
-						name: 'All',
-						value: ''
-					},
-					{
-						name: 'Invoice',
-						value: 'INVOICE'
-					},
-					{
-						name: 'Credit Note',
-						value: 'CREDIT_NOTE'
-					},
-					{
-						name: 'Delivery Note',
-						value: 'DELIVERYNOTE'
-					},
-					{
-						name: 'Medical Receipt',
-						value: 'MEDICAL_RECEIPT'
-					},
-					{
-						name: 'Patient Form',
-						value: 'PATIENT_FORM'
-					},
-					{
-						name: 'Process Form',
-						value: 'PROCESS_FORM'
-					},
-					{
-						name: 'Order Confirmation',
-						value: 'ORDER_CONFIRMATION'
-					},
-					{
-						name: 'Order',
-						value: 'ORDER'
-					},
-					{
-						name: 'ZUGFeRD',
-						value: 'XML_INVOICE'
-					},
-				],
-				default: '',
-				description: 'Set documenttype for extraction',
-				required: false,
-			},
-			{
-				displayName: 'Documentsubtype',
-				name: 'doc_sub_type',
-				type: 'options',
-				displayOptions: {
-					show: {
-						doc_type: [
-							'INVOICE'
-						]
-					},
-				},
-				options: [
-					{
-						name: 'All',
-						value: ''
-					},
-					{
-						name: 'Warenrechnung',
-						value: 'GOODS_INVOICE'
-					},
-					{
-						name: 'Kostenrechnung',
-						value: 'COST_INVOICE'
-					},
-					{
-						name: 'Frachtrechnung',
-						value: 'FREIGHT_INVOICE'
-					},
-				],
-				default: '',
-				description: 'Set document subtype',
-				required: true,
-			},
-      ],
-    };
+	};
 
     async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
         try {
@@ -145,17 +169,26 @@ export class Extract implements INodeType {
             const items = this.getInputData();
             const binaryPropertyName = this.getNodeParameter('binaryPropertyName', 0) as string;
             const credentials = await this.getCredentials('Doc2AppApi') as IDataObject;
+						let doc_type = this.getNodeParameter('doc_type', 0) as string;
+						let doc_sub_type = this.getNodeParameter('doc_sub_type', 0) as string;
+						const document_label = this.getNodeParameter('label', 0) as string ?? 'Test_Label';
             const api_key = credentials.apiKey;
             const formData = {
-                files: [] as UploadFile[]
+                files: [] as UploadFile[],
+								doc_type: doc_type,
+								doc_sub_type: doc_sub_type,
+								label: document_label as string,
             };
 
             for (let i = 0; i < items.length; i++) {
                 const item = items[i].binary as IBinaryKeyData;
-
-                const binaryData = item[binaryPropertyName] as IBinaryData;
-                binaryData.fileName = (i + 1) + 'extract_document.pdf';
-                const dataBuffer = (await this.helpers.getBinaryDataBuffer(i, binaryPropertyName));
+								const binaryData = item[binaryPropertyName] as IBinaryData;
+								const dataBuffer = (await this.helpers.getBinaryDataBuffer(i, binaryPropertyName));
+								if (document_label) {
+									binaryData.fileName = document_label as string;
+								} else {
+									binaryData.fileName = (i + 1) + 'extract_document.pdf';
+								}
 
                 const file = {
                     value: dataBuffer,
