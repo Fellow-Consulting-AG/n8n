63aa85401576de5df1b0fbd491ceebd5a3f1a124
Benjamin Roder <Benjamin.Roder@fellow-consulting.de>
2022-09-12 15:55:33 +0200

    stash

--- packages/nodes-base/nodes/Doc2App/Extract/Extract.node.ts
+++ packages/nodes-base/nodes/Doc2App/Extract/Extract.node.ts
@@ -9,6 +9,10 @@ import {
     INodeType,
     INodeTypeDescription,
     IDataObject,
+		IHookFunctions,
+		ILoadOptionsFunctions,
+		INodePropertyOptions,
+		IHttpRequestOptions
 } from 'n8n-workflow';
 
 import {
@@ -44,11 +48,19 @@ export class Extract implements INodeType {
             },
         ],
         properties: [
+					{
+						displayName: 'Label',
+						name: 'label',
+						type: 'string',
+						default: 'Test_Label',
+						description: 'Label of the Document',
+						required: false,
+					},
           {
 						displayName: 'Binary Property',
 						name: 'binaryPropertyName',
 						type: 'string',
-						default: 'data',
+						default: '',
 						description: 'Object property name which holds binary data.',
 						required: true,
 					},
@@ -56,132 +68,126 @@ export class Extract implements INodeType {
 						displayName: 'Documenttype',
 						name: 'doc_type',
 						type: 'options',
-						options: [
-							{
-								name: 'All',
-								value: ''
-							},
-							{
-								name: 'Invoice',
-								value: 'INVOICE'
-							},
-							{
-								name: 'Credit Note',
-								value: 'CREDIT_NOTE'
-							},
-							{
-								name: 'Delivery Note',
-								value: 'DELIVERYNOTE'
-							},
-							{
-								name: 'Medical Receipt',
-								value: 'MEDICAL_RECEIPT'
-							},
-							{
-								name: 'Patient Form',
-								value: 'PATIENT_FORM'
-							},
-							{
-								name: 'Process Form',
-								value: 'PROCESS_FORM'
-							},
-							{
-								name: 'Order Confirmation',
-								value: 'ORDER_CONFIRMATION'
-							},
-							{
-								name: 'Order',
-								value: 'ORDER'
-							},
-							{
-								name: 'ZUGFeRD',
-								value: 'XML_INVOICE'
-							},
-						],
+						typeOptions: {
+							loadOptionsMethod: 'getDocumentTypes',
+						},
+						description: 'Use documenttype from the list',
 						default: '',
-						description: 'Set documenttype for extraction',
-						required: false,
+						required: true,
 					},
 					{
-						displayName: 'Documentsubtype',
-						name: 'doc_sub_type',
-						type: 'options',
-						displayOptions: {
-							show: {
-								doc_type: [
-									'INVOICE'
-								]
-							},
-						},
-						options: [
-							{
-								name: 'All',
-								value: ''
-							},
-							{
-								name: 'Warenrechnung',
-								value: 'GOODS_INVOICE'
-							},
-							{
-								name: 'Kostenrechnung',
-								value: 'COST_INVOICE'
-							},
-							{
-								name: 'Frachtrechnung',
-								value: 'FREIGHT_INVOICE'
-							},
-						],
+						displayName: 'Documentsource',
+						name: 'documentsource',
+						type: 'string',
 						default: '',
-						description: 'Set document subtype',
-						required: true,
+						description: 'Set document source',
+						required: false,
 					},
         ],
     };
+		methods = {
+			loadOptions: {
+					async getDocumentTypes(this: IHookFunctions | ILoadOptionsFunctions): Promise<INodePropertyOptions[]>{
+							const returnData = [
+									{
+											name: 'All',
+											value: '',
+									}
+							];
+							const credentials = await this.getCredentials('Doc2AppApi') as IDataObject;
+							const api_key = credentials.apiKey;
+							let url = api.get_document_types;
+							const options: IHttpRequestOptions = {
+									url,
+									headers: {
+											'Content-Type': 'application/json',
+											'X-API-KEY': api_key,
+									},
+									method: 'GET',
+									body:  {},
+									json: true,
+							};
 
-    async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-        try {
-            let responseData;
-            const items = this.getInputData();
-            const binaryPropertyName = this.getNodeParameter('binaryPropertyName', 0) as string;
-            const credentials = await this.getCredentials('Doc2AppApi') as IDataObject;
-            const api_key = credentials.apiKey;
-            const formData = {
-                files: [] as UploadFile[]
-            };
+							try {
+									if (this.helpers === undefined) {
+											return returnData;
+									}
+									const response = await this.helpers.httpRequest(options);
+									for (const document_type of response.data) {
+											const docTypeName = document_type.title;
+											const docTypeId = document_type.document_key;
 
-            for (let i = 0; i < items.length; i++) {
-                const item = items[i].binary as IBinaryKeyData;
+											if (document_type.is_active !== true) {
+													continue;
+											}
+											returnData.push({
+													name: docTypeName,
+													value: docTypeId,
+											});
+									}
+							} catch(e: any) {
+									console.error(e);
+									// throw new Error('Some internal error occur. Please try again later');
+							}
 
-                const binaryData = item[binaryPropertyName] as IBinaryData;
-                binaryData.fileName = (i + 1) + 'extract_document.pdf';
-                const dataBuffer = (await this.helpers.getBinaryDataBuffer(i, binaryPropertyName));
+							return returnData;
+					},
+			},
+	};
 
-                const file = {
-                    value: dataBuffer,
-                    options: {
-                        filename: binaryData.fileName,
-                        contentType: binaryData.mimeType,
-                    },
-                } as UploadFile;
-                formData.files.push(file);
-            }
-            let uri = api.process_documents;
-            const options: OptionsWithUri = {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'X-API-KEY': api_key,
-                },
-                method: 'POST',
-                body:  formData ,
-                uri: uri,
-                json: true,
-            };
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		try {
+				let responseData;
+				const items = this.getInputData();
+				const binaryPropertyName = this.getNodeParameter('binaryPropertyName', 0) as string;
+				const credentials = await this.getCredentials('Doc2AppApi') as IDataObject;
+				let doc_type = this.getNodeParameter('doc_type', 0) as string;
+				let source = this.getNodeParameter('documentsource', 0) as string;
+				const document_label = this.getNodeParameter('label', 0) as string ?? 'Test_Label';
+				const api_key = credentials.apiKey;
+				const formData = {
+						files: [] as UploadFile[],
+						doc_type: doc_type,
+						source: source,
+						label: document_label as string,
+				};
 
-            responseData = await this.helpers.request(options);
-            return [this.helpers.returnJsonArray(responseData)];
-        } catch(e: any) {
-            console.error(e);
-            throw new Error('Some internal error occur. Please try again later');
-        }
-    }
+				for (let i = 0; i < items.length; i++) {
+						const item = items[i].binary as IBinaryKeyData;
+						const binaryData = item[binaryPropertyName] as IBinaryData;
+						const dataBuffer = (await this.helpers.getBinaryDataBuffer(i, binaryPropertyName));
+						if (document_label) {
+							binaryData.fileName = document_label as string;
+						} else {
+							binaryData.fileName = (i + 1) + 'extract_document.pdf';
+						}
+
+						const file = {
+								value: dataBuffer,
+								options: {
+										filename: binaryData.fileName,
+										contentType: binaryData.mimeType,
+								},
+						} as UploadFile;
+						formData.files.push(file);
+				}
+				let uri = api.process_documents;
+				const options: OptionsWithUri = {
+						headers: {
+								'Content-Type': 'multipart/form-data',
+								'X-API-KEY': api_key,
+						},
+						method: 'POST',
+						body:  formData ,
+						uri: uri,
+						json: true,
+				};
+
+				responseData = await this.helpers.request(options);
+				return [this.helpers.returnJsonArray(responseData)];
+		} catch(e: any) {
+				console.error(e);
+				throw new Error('Some internal error occur. Please try again later');
+		}
+}
 }
